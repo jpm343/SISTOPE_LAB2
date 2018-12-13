3f7cde839cb3fceef2af8a689c06518925f28458
Vicente <vicente.hernandez@usach.cl>
2018-12-13 01:36:58 -0300

    cambio entre X e Y, y sin prints

--- grilla.h
+++ grilla.h
@@ -5,39 +5,39 @@ typedef struct __casilla_t {
 }casilla_t;
 
 //entradas: medidas de la matriz en X e Y
-casilla_t** grilla_create(int dim_X, int dim_Y){
+casilla_t** grilla_create(int dim_Y, int dim_X){
     casilla_t **new;
     int i, j;
-    new = (casilla_t **)calloc(dim_X, sizeof(casilla_t *) ); // arreglo de punteros de casilla
-    for (i=0; i<dim_X; i++) {
-        new[i] = (casilla_t *)calloc(dim_Y, sizeof(casilla_t) );
-        for(j=0; j<dim_Y; j++) {
+    new = (casilla_t **)calloc(dim_Y, sizeof(casilla_t *) ); // arreglo de punteros de casilla
+    for (i=0; i<dim_Y; i++) {
+        new[i] = (casilla_t *)calloc(dim_X, sizeof(casilla_t) );
+        for(j=0; j<dim_X; j++) {
             pthread_mutex_init(&new[i][j].lock, NULL);
             new[i][j].energia = 0;
         }
     }
-    printf("iniciada grilla\n");
+    //printf("iniciada grilla\n");
     return new;
 }
 
-void fprint_grilla(casilla_t **grilla, int dim_X, int dim_Y){
+void fprint_grilla(casilla_t **grilla, int dim_Y, int dim_X){
     FILE *fp;
     fp = fopen("salida.out", "w");
     int i, j;
-    for(i = 0; i < dim_X; i++){
-        for(j = 0; j < dim_Y; j++){
+    for(i = 0; i < dim_Y; i++){
+        for(j = 0; j < dim_X; j++){
             fprintf( fp, "<%d [%d][%d]>\n", grilla[i][j].energia, i, j);
         }
     }
     fclose(fp);
 }
 
-void grilla_free(casilla_t **grilla, int dim_X, int dim_Y){
+void grilla_free(casilla_t **grilla, int dim_Y, int dim_X){
     int i;
-    for(i = 0; i < dim_X; i++){
+    for(i = 0; i < dim_Y; i++){
         free(grilla[i]);
-        printf("liberado grilla[%d]\n", i);
+        //printf("liberado grilla[%d]\n", i);
     }
     free(grilla);
-    printf("liberada grilla\n");
+    //printf("liberada grilla\n");
 }
